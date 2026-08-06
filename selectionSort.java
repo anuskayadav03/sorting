8ebbd3f2690e6c289d7b99d3de57b511a5186a48
anushkayadav03 <anuska.yadav0303@gmail.com>
2026-08-06 12:38:39 +0530

    selectionSort done

--- selectionSort.java
+++ selectionSort.java
@@ -1,2 +1,21 @@
 public class selectionSort {
+    public static void main(String[] args) {
+        int[] arr={90,3,20,5,60};
+        int n=arr.length;
+        for(int i=0;i<n-1;i++){
+            int min=i;
+            for(int j=i+1;j<n;j++){
+                if(arr[j]<arr[min]){
+                    min=j;
+                }
+            }
+            int temp=arr[i];
+            arr[i]=arr[min];
+            arr[min]=temp;
+        }
+        for(int i=0;i<n;i++) {
+            System.out.print(arr[i] +" ");
+
+        }
+    }
 }

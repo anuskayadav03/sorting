19018bea9d01c9b7725b4a57843736b260d1a4d9
anushkayadav03 <anuska.yadav0303@gmail.com>
2026-08-06 13:32:59 +0530

    bubbleSORT done

---
+++ bubbleSort.java
@@ -0,0 +1,20 @@
+public class bubbleSort {
+    public static void main(String args[]) {
+        int[] arr={2,40,30,1,5,100};
+        int n=arr.length;
+
+        for(int i=0;i<n-1;i++){
+            for(int j=0;j<n-i-1;j++){
+
+                if(arr[j]>arr[j+1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i] +" ");
+        }
+    }
+}

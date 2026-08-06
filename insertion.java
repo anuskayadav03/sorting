84d5f3ce08f1ed9d52ed47711a207a2d0ab1a8fd
anushkayadav03 <anuska.yadav0303@gmail.com>
2026-08-06 15:27:11 +0530

    insetion sort added

--- insertion.java
+++ insertion.java
@@ -1,5 +1,17 @@
 public class insertion {
     public static void main(String[]args){
-
+        int[] arr={9,6,12,8,15,14};
+        for(int i=1;i<arr.length;i++){
+            int j=i;
+         while(j>0&&arr[j-1]>arr[j]){
+             int temp=arr[j-1];
+             arr[j-1]=arr[j];
+             arr[j]=temp;
+             j--;
+         }
+        }
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
     }
 }
